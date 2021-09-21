659d946743732b27fb3ec02192c0e36174f769ce
claudesimo1990 <45970298+claudesimo1990@users.noreply.github.com>
2021-09-21 21:38:59 +0200

    wip

--- resources/views/components/testimonial.blade.php
+++ resources/views/components/testimonial.blade.php
@@ -3,7 +3,7 @@
         <div class="container">
             <div class="intro">
                 <h2 class="text-center">Ce que disent nos utilisateurs</h2>
-                <p class="text-center">Our customers love us! Read what they have to say below. Aliquam sed justo ligula. Vestibulum nibh erat, pellentesque ut laoreet vitae.</p>
+                <p class="text-center"></p>
             </div>
             {{ $slot }}
         </div>
